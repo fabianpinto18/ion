a61b5ed03af9d0610d7f02f03d4d2c2d773c4863
fabian <fabian18pinto@hotmail.com>
2019-05-24 21:53:57 -0500

    actualizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'home', loadChildren: './login/login.module#LoginPageModule' }
+  { path: 'home', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
